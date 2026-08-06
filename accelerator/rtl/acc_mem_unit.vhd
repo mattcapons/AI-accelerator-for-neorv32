5485b2db0b9b5f760e4c448958f7fb0ce2665537
Matteo Capone <matteo.capone11@gmail.com>
2026-08-06 13:03:25 +0200

    Implmented enable in Processing element and rough structure for memory

---
+++ accelerator/rtl/acc_mem_unit.vhd
@@ -0,0 +1,49 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+use work.systolic_pkg.all;
+
+entity acc_mem_unit is
+    generic (
+        MEM_DEPTH : positive := 2*NUM_PE
+    );
+    port (
+        data_i          : in  std_logic_vector(NUM_PE*DATA_WIDTH-1 downto 0);
+        read_addr_i     : in  integer range 0 to MEM_DEPTH-1;
+        write_addr_i    : in  integer range 0 to MEM_DEPTH-1;
+        wr_en_i         : in  std_logic;
+        clk_i           : in  std_logic;
+        rst_i           : in  std_logic;
+        data_o          : out std_logic_vector(NUM_PE*DATA_WIDTH-1 downto 0)
+    );
+end entity acc_mem_unit;
+
+architecture Behavioral of acc_mem_unit is
+
+    type mem_t is array (
+        0 to MEM_DEPTH-1
+    ) of std_logic_vector(NUM_PE*DATA_WIDTH-1 downto 0);
+
+    signal mem_reg : mem_t;
+
+begin
+
+    process(clk_i, rst_i)
+    begin
+        if rst_i = '1' then
+            mem_reg <= (others => (others => '0'));
+            data_o  <= (others => '0');
+
+        elsif rising_edge(clk_i) then
+
+            if wr_en_i = '1' then
+                mem_reg(write_addr_i) <= data_i;
+            end if;
+
+            data_o <= mem_reg(read_addr_i);
+
+        end if;
+    end process;
+
+end architecture Behavioral;
--- accelerator/rtl/systolic_pe.vhd
+++ accelerator/rtl/systolic_pe.vhd
@@ -5,45 +5,44 @@ use work.systolic_pkg.all;
 
 entity systolic_pe is
     port (
-        a_in        : in  signed (DATA_WIDTH-1 downto 0); -- input activation
-        w_in        : in  signed (DATA_WIDTH-1 downto 0); -- input weight
-        clear_i     : in  std_logic; -- synchronous clear signal
-        clk_i       : in  std_logic; 
-        rst_i       : in  std_logic;
-        a_out       : out  signed (DATA_WIDTH-1 downto 0); -- output activation
-        w_out       : out  signed (DATA_WIDTH-1 downto 0); -- output weight
-        p_sum_out   : out  signed (ACC_WIDTH-1 downto 0) -- output sum
+        a_in        : in signed (DATA_WIDTH-1 downto 0); -- input activation
+        w_in        : in signed (DATA_WIDTH-1 downto 0); -- input weight
+        clear_i     : in std_logic; -- synchronous clear signal
+        en_i        : in std_logic; -- enable signal
+        clk_i       : in std_logic;
+        rst_i       : in std_logic;
+        a_out       : out signed (DATA_WIDTH-1 downto 0); -- output activation
+        w_out       : out signed (DATA_WIDTH-1 downto 0); -- output weight
+        p_sum_out   : out signed (ACC_WIDTH-1 downto 0) -- output sum
     );
 end systolic_pe;
 
 architecture Behavioral of systolic_pe is
-    signal a_reg : signed (DATA_WIDTH-1 downto 0) := (others => '0');
-    signal w_reg : signed (DATA_WIDTH-1 downto 0) := (others => '0');
-    signal p_sum : signed(ACC_WIDTH-1 downto 0) := (others => '0');
+    signal acc_reg : signed(ACC_WIDTH-1 downto 0) := (others => '0');
 begin
     process(clk_i, rst_i)
     begin
-        -- normal reset behavior
+        -- asynchronous reset
         if rst_i = '1' then
-            p_sum <= (others => '0');
-            a_reg <= (others => '0');
-            w_reg <= (others => '0');
+            acc_reg <= (others => '0');
+            a_out <= (others => '0');
+            w_out <= (others => '0');
 
         elsif rising_edge(clk_i) then
             -- synchronous clear behavior
             if clear_i = '1' then
-                p_sum <= (others => '0');
-                a_reg <= (others => '0');
-                w_reg <= (others => '0');
+                acc_reg <= (others => '0');
+                a_out <= (others => '0');
+                w_out <= (others => '0');
             else
-                a_reg <= a_in;
-                w_reg <= w_in;
-                p_sum <= p_sum + resize(a_in * w_in, ACC_WIDTH); -- accumulate the product of a_in and w_in into p_sum
+                if en_i = '1' then
+                    a_out <= a_in;
+                    w_out <= w_in;
+                    acc_reg <= acc_reg + resize(a_in * w_in, ACC_WIDTH); -- accumulate the product of a_in and w_in into p_sum
+                end if;
             end if;
         end if;
     end process;
 
-    a_out <= a_reg;
-    w_out <= w_reg;
-    p_sum_out <= p_sum;
+    p_sum_out <= acc_reg;
 end Behavioral;
